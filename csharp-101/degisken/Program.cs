c911cd724b3ec07a5e56046b2a67142eb66c2e5e
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-10 19:21:54 +0300

    Add degisken project

---
+++ csharp-101/degisken/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace degisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 5;
+            sbyte sb = -3;
+            short shrt = -2;
+            ushort ushrt = 2;
+
+            Int16 i16 = 2;
+            int i = 2;
+            Int32 i32 = 2;
+            Int64 i64 = 2;
+
+            uint ui = 2;
+            long l = 2;
+            ulong ul = 2;
+
+            float fl = 4;
+            double db = 3;
+            decimal dc = 334;
+
+            char c = 'c';
+            string s = "string";
+
+            DateTime dt = DateTime.Now;
+
+            object o1 = 2;
+
+            Console.WriteLine(dt);
+        }
+    }
+}
