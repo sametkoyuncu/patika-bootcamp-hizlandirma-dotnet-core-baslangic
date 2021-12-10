3ca326fa985de6ce471919bba68dff1346e3275f
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-10 19:26:25 +0300

    Add operatorler project

---
+++ csharp-101/operatorler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operatörler");
+        }
+    }
+}
