0bb889dd85da52abe358e02c825891dc2e7557fa
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-13 13:16:43 +0300

    Add switch-case project

---
+++ csharp-101/switch-case/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degisken = 2;
+            switch (degisken)
+            {
+                case sabit1:
+                    // komutlar;
+                    break;
+                case sabit2:
+                    // komutlar;
+                    break;
+                case sabit3:
+                    // komutlar;
+                    break;
+                default:
+                    // komutlar;
+                    break;
+            }
+        }
+    }
+}
