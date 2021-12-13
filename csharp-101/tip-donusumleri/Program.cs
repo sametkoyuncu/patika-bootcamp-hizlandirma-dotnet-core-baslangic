b3705b3e078157882123cd0b07f131d0368330c2
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-13 12:44:49 +0300

    Add conversion examples

--- csharp-101/tip-donusumleri/Program.cs
+++ csharp-101/tip-donusumleri/Program.cs
@@ -7,6 +7,18 @@ namespace tip_donusumleri
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Implicit Conversion
+            float a;
+            int b = 25;
+            a = b;
+
+            short x = 10;
+            int y;
+            y = x;
+
+            // Explicit Conversion
+            int a2 = 2323;
+            byte b2 = a2;
         }
     }
 }
