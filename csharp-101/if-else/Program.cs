00a6b1bf5756b2d9b28d9c3ba0698a2b5a1ce5cb
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-13 13:12:36 +0300

    Add if-else project

---
+++ csharp-101/if-else/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+
+            if (condition)
+            {
+                Console.WriteLine("Değişken: true");
+            }
+            else
+            {
+                Console.WriteLine("Değişken: false");
+            }
+        }
+    }
+}
