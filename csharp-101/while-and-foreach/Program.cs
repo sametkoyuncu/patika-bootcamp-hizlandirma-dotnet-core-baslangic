69af6aac2ea8a5ee35cc5c8cfb4f85847186cfac
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-13 13:33:49 +0300

    Add while and foreach project

---
+++ csharp-101/while-and-foreach/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace while_and_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] diziAdı = { 2, 4, 5, 1, 2, 4 };
+            int sayac = 1;
+            while (sayac <= 5)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+
+            foreach (değişkenTipi değişkenAdı in diziAdı)
+            {
+                //Komutlar
+            }
+
+            int[] sayiDizisi = { 2, 4, 5, 1, 2, 4 };
+            int toplam = 0;
+
+            foreach (int i in SayiDizisi)
+            {
+                toplam += i;
+            }
+        }
+    }
+}
