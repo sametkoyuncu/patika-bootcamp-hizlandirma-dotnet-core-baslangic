b576645bf140453145e108f77a0d76bd2f2c24b1
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-13 13:29:59 +0300

    Add for-loop

---
+++ csharp-101/for-loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i; ;)// Sonsuz döngü
+            {
+                a = Convert.ToChar(Console.ReadLine());
+                if (i == 7)
+                    break;
+            }
+            //Kod burdan devam eder. 
+
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
