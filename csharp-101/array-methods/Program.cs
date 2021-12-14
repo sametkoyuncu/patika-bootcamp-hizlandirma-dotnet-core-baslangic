a605ef663aad44c08771abf31761a1d480a3a874
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-14 19:38:45 +0300

    Fix clear method

--- csharp-101/array-methods/Program.cs
+++ csharp-101/array-methods/Program.cs
@@ -8,7 +8,7 @@ namespace array_methods
         {
             int[] sayiDizisi = { 1, 3, 4, 9, 8, 7 };
             Array.Sort(sayiDizisi);
-            Array.Clear(sayiDizisi, 2, 2); // 2. indexten 2 eleman sil
+            Array.Clear(sayiDizisi, 2, 2); // 2. indexten 2 eleman sıfırla
             Array.Reverse(sayiDizisi);
             Array.IndexOf(sayiDizisi, 7);
             Array.Resize<int>(ref sayiDizisi, 12); // 12 elemanlı yapar
