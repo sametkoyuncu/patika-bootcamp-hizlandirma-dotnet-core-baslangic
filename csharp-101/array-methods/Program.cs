3059a1d8e5cafdc19da9756db9a59b390640abf3
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-14 19:38:11 +0300

    Add array methods project

---
+++ csharp-101/array-methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace array_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = { 1, 3, 4, 9, 8, 7 };
+            Array.Sort(sayiDizisi);
+            Array.Clear(sayiDizisi, 2, 2); // 2. indexten 2 eleman sil
+            Array.Reverse(sayiDizisi);
+            Array.IndexOf(sayiDizisi, 7);
+            Array.Resize<int>(ref sayiDizisi, 12); // 12 elemanlı yapar
+            sayiDizisi[6] = 10;
+            Array.Resize<int>(ref sayiDizisi, 3); // 3 elemana indirir
+        }
+    }
+}
