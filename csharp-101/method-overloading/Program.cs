9f6b9ee166a100ac4c04be6dafb41c0f2a48c885
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-15 13:14:42 +0300

    Add method overloading project

---
+++ csharp-101/method-overloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace method_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toplam(4, 8, out int toplam);
+            Console.WriteLine(toplam);
+        }
+
+        public void EkranaYazdir(int deger)
+        {
+            Console.WriteLine(deger);
+        }
+
+        public void EkranaYazdir(string deger)
+        {
+            Console.WriteLine(deger);
+        }
+
+        public void EkranaYazdir(string deger1, string deger2)
+        {
+            Console.WriteLine(deger1 + deger2);
+        }
+
+
+
+        public static int Toplam(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+            return toplam;
+        }
+    }
+}
