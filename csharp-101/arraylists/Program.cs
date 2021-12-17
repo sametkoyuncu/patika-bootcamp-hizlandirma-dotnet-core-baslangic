9acaa942bfe4a2034126ac0b71685cdc95fdadaa
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-17 20:57:44 +0300

    Add arraylists project

---
+++ csharp-101/arraylists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace arraylists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            liste.Add("Ali");
+            liste.Add(4);
+            liste.Add(true);
+            liste.Add('A');
+
+            // AddRamge
+            liste.AddRange("Mor", 2, 'B');
+
+            // Sort
+            // liste.Sort();
+
+            // BinarySearch - önce sıralayıp sonra bunun kullanmamız gerekiyor
+            liste.BinarySearch("Mor");
+
+            // Reverse - ters çevir
+            liste.Reverse();
+
+
+        }
+    }
+}
