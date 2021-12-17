eeeb95a1637362344639129574588ee449f7bae5
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-17 21:10:37 +0300

    Add dictionaries project

---
+++ csharp-101/dictionaries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // key value ilişkisi var, key'ler unique
+            Dictionary<int, string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(10, "Veli Kağan");
+            kullanicilar.Add(12, "Ezgi Çetin");
+            kullanicilar.Add(13, "Ali Kemal");
+
+            Console.WriteLine(kullanicilar[12]);
+
+            // Count
+
+            // ContainsValue ve ContainsKey, true veya false döner
+
+            // Remove
+
+            // Keys ve Values
+            // kullanicilar.Keys  gibi foreach yer
+        }
+    }
+}
