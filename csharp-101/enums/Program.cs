c7b8031295445bcbfaf4306896fea47684f2599b
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-18 21:26:36 +0300

    Add 'enums' project

---
+++ csharp-101/enums/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Salı);
+            Console.WriteLine((int)Gunler.Pazar); // numerik değerini alma
+
+            int sıcaklık = 32;
+
+            if (sıcaklık <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyin.");
+            else if (sıcaklık >= (int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya çıkmak için hava çok sıcak.");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi, // normalde 0'dan başlar ama biz değer de atayabiliriz
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma = 23,
+        Cumartesi, // ardışık gideceği için cumaya kadar 0 1 2 3, cuma 23, sonrası  24, 25
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
